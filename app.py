bdcd2284c3eb77e095bdc3eca49756868babe964
fergus <fergus.jingtai@gmail.com>
2019-05-17 02:27:03 +0800

    使用 for 迴圈產生文章內容

--- app.py
+++ app.py
@@ -1,9 +1,51 @@
 from flask import Flask, render_template
+from datetime import datetime
+
 app = Flask(__name__)
+
+posts_data = [
+  {
+    'id': '111',
+    'title': '關於蝦皮 1元奪寶戰',
+    'content': '111內容',
+    'category': '網路購物',
+    'creattime': datetime.strptime('2019-05-01', '%Y-%m-%d')
+  }, {
+    'id': '222',
+    'title': '關於蝦皮 1元奪寶戰',
+    'content': '222內容',
+    'category': '網路購物',
+    'creattime': datetime.strptime('2019-05-02', '%Y-%m-%d')
+  }, {
+    'id': '333',
+    'title': '關於蝦皮 1元奪寶戰',
+    'content': '333內容',
+    'category': '網路購物',
+    'creattime': datetime.strptime('2019-05-03', '%Y-%m-%d')
+  }, {
+    'id': '444',
+    'title': '關於蝦皮 1元奪寶戰',
+    'content': '444內容',
+    'category': '網路購物',
+    'creattime': datetime.strptime('2019-05-04', '%Y-%m-%d')
+  }, {
+    'id': '555',
+    'title': '關於蝦皮 1元奪寶戰',
+    'content': '555內容',
+    'category': '網路購物',
+    'creattime': datetime.strptime('2019-05-05', '%Y-%m-%d')
+  }, {
+    'id': '666',
+    'title': '關於蝦皮 1元奪寶戰',
+    'content': '666內容',
+    'category': '網路購物',
+    'creattime': datetime.strptime('2019-05-06', '%Y-%m-%d')
+  },
+]
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return render_template('index.html', posts=posts_data)
 
 @app.route('/posts/<id>')
 def posts(id):
